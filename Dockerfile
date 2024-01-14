e612e507f75d5e3744f99f0917767d791a3c3a29
alrnaldar <deccorater@mail.ru>
2024-01-14 11:50:56 +0900

    docker

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,19 @@ WORKDIR /app
 
 COPY . .
 
-COPY requirements.txt .
 
-RUN pip install --no-cache-dir -r requirements.txt
+
 
 CMD [ "python", "main.py"]
 
 FROM python:3.11-alpine
 
 WORKDIR /app
+RUN pip install --upgrade pip
 
 RUN apk --no-cache add libc6-compat
-
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
 COPY --from=builder /app .
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message, ReplyKeyboardMarkup,KeyboardButton
 from aiogram.filters import Command
+from config import TOKEN
 
-
-bot = Bot(token=os.getenv("API_TOKEN"))
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
+@dp.message(lambda message: message.text == "цитата")
+async def quote_process(msg:Message):
+    await msg.answer("aaaa")
+if __name__ == "__main__":
+    dp.run_polling(bot) 
